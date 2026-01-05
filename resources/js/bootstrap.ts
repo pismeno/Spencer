6aa7c64633ee299ea150fbe933ea8e3dd1d5757e
pismeno <kyky151cze@gmail.com>
2026-01-05 22:46:59 +0100

    test

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -4,7 +4,6 @@ const axiosInstance = axios.create({
     headers: { 'X-Requested-With': 'XMLHttpRequest' },
 });
 
-// Attach CSRF token if present (Laravel default meta tag)
 const token = document.head?.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
 if (token) {
     axiosInstance.defaults.headers.common['X-CSRF-TOKEN'] = token;
