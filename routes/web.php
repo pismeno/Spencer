a71354ba080d8c12df95f2353ad03c9be69e83a0
pismeno <kyky151cze@gmail.com>
2026-01-05 21:40:39 +0100

    Testing Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('test');
 });
+
+Route::post('/submit', [TestController::class, 'test']);
--- storage/framework/views/db541439f316b36da25d152621f6e69c.php
+++
@@ -1,4 +0,0 @@
-<?php
-?>
-<h1>Test215</h1>
-<?php /**PATH D:\programs\xampp\htdocs\Spencer\resources\views/test.blade.php ENDPATH**/ ?>
