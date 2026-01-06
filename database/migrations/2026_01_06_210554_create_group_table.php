d6eb143ac501c5c15334093e6cb289a166a81c0c
pismeno <kyky151cze@gmail.com>
2026-01-06 22:13:09 +0100

    Created migrations for Spencer database

---
+++ database/migrations/2026_01_06_210554_create_group_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->increments('group_id');
+            $table->string('name', 128);
+            $table->text('description')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group');
+    }
+};
