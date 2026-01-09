0dcbe313246807d5f9b7866144619135d013e373
pismeno <kyky151cze@gmail.com>
2026-01-09 11:21:29 +0100

    middleware protection for pages that should be only accessible after loggin in

--- routes/web.php
+++ routes/web.php
@@ -5,33 +5,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->middleware('auth');;
 
 Route::get('/event', function () {
     return view('event');
-});
+})->middleware('auth');;
 
 Route::get('/group', function () {
     return view('group');
-});
+})->middleware('auth');;
 
 Route::get('/notifications', function () {
     return view('notifications');
-});
+})->middleware('auth');;
 
 Route::get('/settings', function () {
     return view('settings');
-});
+})->middleware('auth');;
 
 
 // AUTHENTICATION ROUTES
 Route::get('/register', function () {
     return view('auth/register');
-});
+})->name('register');
 
 Route::get('/login', function () {
     return view('auth/login');
-});
+})->name('login');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
