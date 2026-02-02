3f82f3bd5318006c8cb43bbf6d6f4ec5967dec7c
Ma2mus <barcama24@sps-prosek.cz>
2026-02-02 21:46:31 +0100

    Event handler partially finished

---
+++ resources/js/createEvent.ts
@@ -0,0 +1,54 @@
+import api from '@/bootstrap';
+const title = document.getElementById("input-title") as HTMLInputElement;
+const description = document.getElementById("input-description") as HTMLTextAreaElement;
+const deadline = document.getElementById("input-deadline") as HTMLInputElement;
+const from = document.getElementById("input-from") as HTMLInputElement;
+const to = document.getElementById("input-to") as HTMLInputElement;
+const img = document.getElementById("input-img") as HTMLInputElement;
+const submitBtn = document.getElementById("save-changes") as HTMLButtonElement;
+
+
+
+
+submitBtn.addEventListener("click", async (e)=>{
+    e.preventDefault();
+    if (!title?.value.trim()) {
+        console.error("title je povinny");
+        title?.focus();
+        return;
+    }
+    if (!deadline?.value) {
+        console.error("deadline je povinny");
+        deadline?.focus();
+        return;
+    }
+    if (!from?.value) {
+        console.error("Event musí někdy začít");
+        from?.focus();
+        return;
+    }
+    if (!to?.value) {
+        console.error("Event musi nekdy koncit");
+        to?.focus();
+        return;
+    }
+    const formData = new FormData();
+    formData.append("title", title.value.trim());
+    formData.append("description",description.value.trim());
+    formData.append("deadline", deadline.value);
+    formData.append("from", from.value);
+    formData.append("to", to.value);
+    if (img.files && img.files?.[0]) {
+        formData.append("img", img.files?.[0])
+    }
+    try{
+        submitBtn.disabled=true;
+        const response = await api.post("/createEvent", formData);
+        window.location.href="/";
+    }catch (error:unknown){
+        console.error("Error: "+error);
+        submitBtn.disabled=false;
+    }
+})
+
+
---
+++ resources/js/showImg.ts
@@ -0,0 +1,70 @@
+const isValidImage = (file:File):Promise<boolean> => { 
+    return new Promise(resolve => {
+        const img = new Image();
+        const url = URL.createObjectURL(file);
+
+        img.onload = () => {
+            URL.revokeObjectURL(url);
+            resolve(true);
+        }
+        img.onerror = () =>{
+            URL.revokeObjectURL(url);
+            resolve(false);  
+        } 
+
+        img.src = url;
+});
+}
+
+const setupImageInput = () => {
+    let urlImage:string | null = null
+    const input = document.getElementById("event-image-upload") as HTMLInputElement;
+    if (!input) {
+        console.error("Input nebyl na strance nalezen");
+        return;
+    }
+    input.addEventListener("change", async (event:Event) =>{
+        const target = event.target as HTMLInputElement;
+        const file = target.files?.[0];
+        if (urlImage) {
+            URL.revokeObjectURL(urlImage)
+            urlImage = null;
+        }
+        if (file) {
+            const isImgValid = await isValidImage(file);
+            if (isImgValid) {
+                const imgPreview = document.getElementById("img-preview") as HTMLImageElement;
+                if (imgPreview) {
+                    const uploadImg= URL.createObjectURL(file);
+                    urlImage = uploadImg; //  URL object only makes 1 img at the same to due to UX
+                    imgPreview.src = uploadImg;
+                    imgPreview.classList.remove("d-none");   
+                    if (!document.getElementById("remove-thumbnail-btn")) {
+                        const createRemoveThumbnailMessage = document.createElement("span") as HTMLSpanElement;
+                        createRemoveThumbnailMessage.classList.add("d-block", "text-end", "mt-2", "fs-6", "text-danger", "mb-3", "cursor-pointer", "hover-underline"); 
+                        createRemoveThumbnailMessage.id = "remove-thumbnail-btn";
+                        createRemoveThumbnailMessage.textContent = "Remove Thubnail";
+                        createRemoveThumbnailMessage.addEventListener("click", () =>{
+                            imgPreview.classList.add("d-none");
+                            createRemoveThumbnailMessage?.remove();
+                            imgPreview.src = "";
+                            URL.revokeObjectURL(uploadImg);
+                            target.value="";
+                        })
+                        const previewDiv = document.querySelector("#img-preview-div") as HTMLDivElement;
+                        if (previewDiv) {
+                            previewDiv.insertAdjacentElement("afterend", createRemoveThumbnailMessage);
+                        }
+                        
+                    }
+
+                }
+            } else{
+                console.error("chyba blby dormat");
+            }
+        } else{
+            console.error("fiel neni file");
+        }
+    })
+}
+setupImageInput();
