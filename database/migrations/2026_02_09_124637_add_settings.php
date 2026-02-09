ff412e182cb4f174dca75ebd8dd2b7194384b261
patrishr19 <kepp3.yt@gmail.com>
2026-02-09 13:53:53 +0100

    Setting migrations fix

---
+++ database/migrations/2026_02_09_124637_add_settings.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       $themeId = DB::table('settings')->insertGetId([
+            'name' => 'theme',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+
+        DB::table('setting_options')->insert([
+            ['setting_id' => $themeId, 'option_data' => 'light', 'created_at' => now()],
+            ['setting_id' => $themeId, 'option_data' => 'dark', 'created_at' => now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('setting_options');
+        Schema::dropIfExists('settings');
+    }
+};
