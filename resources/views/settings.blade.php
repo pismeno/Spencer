2feba8e2d4752e1552d84ef369374143f3d41680
Ma2mus <barcama24@sps-prosek.cz>
2026-02-24 23:14:36 +0100

    added resources to settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Stack+Sans+Text:wght@200..700&display=swap" rel="stylesheet">
 
-    @vite(['resources/css/custom.css'])
+    @vite(['resources/css/custom.css', 'resources/js/settingsUser.ts'])
 </head>
 <body class="bg-light">
 <x-header />
