91390978f4794de9825c77bbc070d2dfcc0d7634
Ma2mus <barcama24@sps-prosek.cz>
2026-03-01 12:30:38 +0100

    added show events

--- resources/views/showEvent.blade.php
+++ resources/views/showEvent.blade.php
@@ -8,12 +8,22 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Stack+Sans+Text:wght@200..700&display=swap" rel="stylesheet">
-    @vite(['resources/css/custom.css'])
+    @vite(['resources/css/custom.css', 'resources/js/loadEvents.ts'])
 </head>
-<body class="bg-light" data-bs-theme="{{ $activeTheme }}">
-<x-header />
+<body class="bg-light">
+<x-header/>
 <main class="d-flex">
     <x-sidebar/>
+    <div id="content" class="flex-grow-1 p-3 p-md-5 mb-sm-5 mb-md-0 overflow-auto">
+        <div class="container-fluid d-flex gap-4 flex-md-row flex-column">
+            <div class="column g-4 col-md-12">
+                <h2 id="newest-event-title" class="mb-4">Newest Events</h2>
+                <div id="container-events" data-url="{{ Vite::asset('resources/svg/clock.svg') }}">
+
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
